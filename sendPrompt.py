daa129ca884dcea1e92cd82e6fe2df304c023f5e
syanification <syanification@gmail.com>
2025-01-18 10:37:13 -0500

    Can send messages to model for parsing

---
+++ sendPrompt.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from gptConfig import API_KEY
+client = OpenAI(api_key=API_KEY)
+
+def getCommand(input):
+    completion = client.chat.completions.create(
+        model="ft:gpt-4o-mini-2024-07-18:personal::Ar4dngUA",
+        messages=[
+            {"role": "user", "content": input}
+        ]
+    )
+
+    return completion.choices[0].message.content
+
+
+print(getCommand("Turn on the song holy star by grandmaster"))
